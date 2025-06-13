c1eb06c17e903552739b2fe657a1034a04eff2a1
LET <tsumototakuya@LETM3.local>
2025-06-14 08:42:08 +0900

    first commit

---
+++ action/actionApp.swift
@@ -0,0 +1,17 @@
+//
+//  actionApp.swift
+//  action
+//
+//  Created by 津本拓也 on 2025/06/14.
+//
+
+import SwiftUI
+
+@main
+struct actionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
